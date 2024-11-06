19abd5b6d4d4336d8a13740bc4398f43926b1dcf
Mahmoud <lamm.mah2002@gmail.com>
2024-11-06 20:32:12 +0100

    v1.1.1

--- myscript.js
+++ myscript.js
@@ -2,3 +2,6 @@ document.querySelector("button").addEventListener("click", function() {
     document.getElementById("h1").innerText = "Button Clicked!";
     alert("Button was clicked!");
 });
+function hello(params) {
+    
+}
